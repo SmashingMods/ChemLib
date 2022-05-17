c56ea68f7fa7780dcb754b0b0c6d033ba90b39fb
Andrew Sampson <andrew@akliz.net>
2022-05-16 22:56:05 -0500

    "cleaning" or something. Honestly, it's fine.

--- src/main/java/com/smashingmods/chemlib/client/ElementRenderer.java
+++ src/main/java/com/smashingmods/chemlib/client/ElementRenderer.java
@@ -25,19 +25,15 @@ import java.util.function.Supplier;
 public class ElementRenderer extends BlockEntityWithoutLevelRenderer {
 
 	public static final Supplier<BlockEntityWithoutLevelRenderer> INSTANCE = Suppliers.memoize(
-			() -> new ElementRenderer(
-					Minecraft.getInstance().getBlockEntityRenderDispatcher(), Minecraft.getInstance().getEntityModels()
-			)
+			() -> new ElementRenderer(Minecraft.getInstance().getBlockEntityRenderDispatcher(), Minecraft.getInstance().getEntityModels())
 	);
 	public static final IItemRenderProperties RENDERER = new IItemRenderProperties()
 	{
 		@Override
-		public BlockEntityWithoutLevelRenderer getItemStackRenderer()
-		{
+		public BlockEntityWithoutLevelRenderer getItemStackRenderer() {
 			return INSTANCE.get();
 		}
 	};
-
 	private static final ModelResourceLocation MODEL_RESOURCE_LOCATION = new ModelResourceLocation(new ResourceLocation(ChemLib.MODID, "element_model"), "inventory");
 
 	public ElementRenderer(BlockEntityRenderDispatcher pBlockEntityRenderDispatcher, EntityModelSet pEntityModelSet) {
@@ -46,19 +42,20 @@ public class ElementRenderer extends BlockEntityWithoutLevelRenderer {
 
 	@Override
 	public void renderByItem(ItemStack pStack, ItemTransforms.TransformType pTransformType, PoseStack pPoseStack, MultiBufferSource pBuffer, int pPackedLight, int pPackedOverlay) {
-		pPoseStack.pushPose();
-		pPoseStack.translate(0.5D, 0.5D, 0D);
+
 		boolean gui = pTransformType == ItemTransforms.TransformType.GUI;
 		boolean frame = pTransformType == ItemTransforms.TransformType.FIXED;
-		if (gui)
-			Lighting.setupForFlatItems();
 		BakedModel model = Minecraft.getInstance().getItemRenderer().getItemModelShaper().getModelManager().getModel(MODEL_RESOURCE_LOCATION);
+
 		pPoseStack.pushPose();
+		pPoseStack.translate(0.5D, 0.5D, 0D);
+		if (gui) {
+			Lighting.setupForFlatItems();
+		}
+		pPoseStack.pushPose();
+
 		switch (pTransformType) {
-			case THIRD_PERSON_LEFT_HAND -> {
-				pPoseStack.translate(0, -0.2D, 0.45D);
-			}
-			case THIRD_PERSON_RIGHT_HAND -> {
+			case THIRD_PERSON_LEFT_HAND, THIRD_PERSON_RIGHT_HAND -> {
 				pPoseStack.translate(0, -0.2D, 0.45D);
 			}
 			case FIRST_PERSON_LEFT_HAND -> {
@@ -87,6 +84,7 @@ public class ElementRenderer extends BlockEntityWithoutLevelRenderer {
 				pPoseStack.translate(0, 0, -0.5D);
 			}
 		}
+
 		Minecraft.getInstance().getItemRenderer().render(
 				pStack,
 				pTransformType,
@@ -97,21 +95,23 @@ public class ElementRenderer extends BlockEntityWithoutLevelRenderer {
 				gui ? OverlayTexture.NO_OVERLAY : pPackedOverlay,
 				ForgeHooksClient.handleCameraTransforms(pPoseStack, model, pTransformType, false));
 		pPoseStack.popPose();
+
 		if (gui || frame) {
 			pPoseStack.pushPose();
 			pPoseStack.mulPose(Vector3f.XN.rotation(180));
 			pPoseStack.translate(-0.16D, 0, -0.55D);
 			pPoseStack.scale(0.05F, 0.08F, 0.08F);
+
 			if (frame) {
 				pPoseStack.mulPose(Vector3f.YN.rotationDegrees(180));
 				pPoseStack.mulPose(Vector3f.XN.rotationDegrees(53));
 				pPoseStack.translate(-8D, -1D, 1.7D);
 				pPoseStack.scale(1F, 0.65F, 1F);
 			}
-			Minecraft.getInstance().font.drawShadow(pPoseStack, ((ElementItem) pStack.getItem()).getAbbreviation(), -6, 0, 0xFFFFFF);
+
+			Minecraft.getInstance().font.drawShadow(pPoseStack, ((ElementItem) pStack.getItem()).getAbbreviation(), -5, 0, 0xFFFFFF);
 			pPoseStack.popPose();
 		}
 		pPoseStack.popPose();
 	}
-
 }

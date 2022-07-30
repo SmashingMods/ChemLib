10ad4669350b8d95750012f342147ad2c9253371
Andrew Sampson <andrew@akliz.net>
2022-07-29 19:23:42 -0500

    Add keratin. Add ifPresent checks in chemical registry for optionals. Collapse redundant matterstate case. Update gradle script. Update changelog for release. Bump mod version.

--- changelog.md
+++ changelog.md
@@ -1,3 +1,5 @@
-# Changes
+# ChemLib 1.18.2-2.0.5
 
-- Add some changes to compounds for recipes in Alchemistry.
+- Fix issues 200-201: element classification changed so that elements 72-80 are not marked as Lanthanides. Lanthanides are no longer indicated as radioactive in the periodic table.
+- Add copper, iron, and gold dusts/plates. The base game already adds nuggets, ingots, and blocks.
+- Add keratin for alchemistry recipes that require it.
--- src/main/java/com/smashingmods/chemlib/registry/ChemicalRegistry.java
+++ src/main/java/com/smashingmods/chemlib/registry/ChemicalRegistry.java
@@ -57,7 +57,7 @@ public class ChemicalRegistry {
                                 ItemRegistry.registerItemByType(registryObject, ChemicalItemType.NUGGET, ItemRegistry.METALS_TAB);
                                 ItemRegistry.registerItemByType(registryObject, ChemicalItemType.INGOT, ItemRegistry.METALS_TAB);
                                 BlockRegistry.BLOCKS.register(String.format("%s_metal_block", elementName), () -> new ChemicalBlock(new ResourceLocation(ChemLib.MODID, elementName), ChemicalBlockType.METAL, BlockRegistry.METAL_BLOCKS, BlockRegistry.METAL_PROPERTIES));
-                                ItemRegistry.fromChemicalBlock(BlockRegistry.getRegistryObjectByName(String.format("%s_metal_block", elementName)).get(), new Item.Properties().tab(ItemRegistry.METALS_TAB));
+                                BlockRegistry.getRegistryObjectByName(String.format("%s_metal_block", elementName)).ifPresent(block -> ItemRegistry.fromChemicalBlock(block, new Item.Properties().tab(ItemRegistry.METALS_TAB)));
                             }
                         }
                         ItemRegistry.registerItemByType(registryObject, ChemicalItemType.DUST, ItemRegistry.METALS_TAB);
@@ -81,13 +81,10 @@ public class ChemicalRegistry {
                                     .color((int) Long.parseLong(color, 16));
 
                             switch (matterState) {
-                                case LIQUID -> {
-                                    FluidRegistry.registerFluid(elementName, attributes, slopeFindDistance, decreasePerBlock);
-                                }
-                                case GAS -> {
+                                case LIQUID, GAS -> {
                                     if (group == 18) {
                                         BlockRegistry.BLOCKS.register(String.format("%s_lamp_block", elementName), () -> new LampBlock(new ResourceLocation(ChemLib.MODID, elementName), ChemicalBlockType.LAMP, BlockRegistry.LAMP_BLOCKS, BlockRegistry.LAMP_PROPERTIES));
-                                        ItemRegistry.fromChemicalBlock(BlockRegistry.getRegistryObjectByName(String.format("%s_lamp_block", elementName)).get(), new Item.Properties().tab(ItemRegistry.MISC_TAB));
+                                        BlockRegistry.getRegistryObjectByName(String.format("%s_lamp_block", elementName)).ifPresent(block -> ItemRegistry.fromChemicalBlock(block, new Item.Properties().tab(ItemRegistry.MISC_TAB)));
                                     }
                                     attributes.gaseous();
                                     FluidRegistry.registerFluid(elementName, attributes, slopeFindDistance, decreasePerBlock);

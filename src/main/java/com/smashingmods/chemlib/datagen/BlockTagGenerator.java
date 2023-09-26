266600badfe880d99b49be661db027e3e1cb28dc
Andrew Sampson <andrew@akliz.net>
2023-09-26 17:51:27 -0500

    Cherrypick from PR #56.

--- src/main/java/com/smashingmods/chemlib/datagen/BlockTagGenerator.java
+++ src/main/java/com/smashingmods/chemlib/datagen/BlockTagGenerator.java
@@ -1,15 +1,20 @@
 package com.smashingmods.chemlib.datagen;
 
 import com.smashingmods.chemlib.ChemLib;
+import com.smashingmods.chemlib.api.MatterState;
 import com.smashingmods.chemlib.registry.BlockRegistry;
 import net.minecraft.data.DataGenerator;
+import com.smashingmods.chemlib.registry.ItemRegistry;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.tags.TagKey;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.common.data.ForgeRegistryTagsProvider;
 import net.minecraftforge.registries.ForgeRegistries;
 
 import javax.annotation.Nonnull;
+import java.util.Objects;
 
 public class BlockTagGenerator extends ForgeRegistryTagsProvider<Block> {
 
@@ -23,11 +28,19 @@ public class BlockTagGenerator extends ForgeRegistryTagsProvider<Block> {
             tag(BlockTags.MINEABLE_WITH_PICKAXE).add(blockRegistryObject.get());
             tag(BlockTags.NEEDS_STONE_TOOL).add(blockRegistryObject.get());
         });
+
+        ItemRegistry.getChemicalBlockItems().forEach(item -> {
+            if (item.getMatterState().equals(MatterState.SOLID)) {
+                String name = item.getChemicalName();
+                TagKey<Block> key = Objects.requireNonNull(ForgeRegistries.BLOCKS.tags()).createTagKey(new ResourceLocation("forge", String.format("storage_blocks/%s", name)));
+                tag(key).add(item.getBlock());
+            }
+        });
     }
 
     @Override
     @Nonnull
     public String getName() {
-        return null;
+        return "";
     }
 }
